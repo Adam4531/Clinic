3d094d4a57c9f22bcfddffe197cf92b841a4be57
Cichyy22 <ok.kuba@wp.pl>
2023-04-28 14:51:44 +0200

    fixing files and sorting

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import RootLayout from './UI/RootLayout';
+import ErrorPage from './misc/Error';
+import HomePage from './misc/HomePage'
+import CrewPage from './misc/Crew'
+import MakeAppointmentPage from './patient/MakeAppointment'
+import Visits from './patient/Visits'
+import RecommendationsPage from './patient/Recommendations'
+import LoginPage from './auth/LoginPage'
+import DetailVisit from './patient/DetailVisit'
+import RecommendationDetail from './patient/RecommendationDetail'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    id: 'root',
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'crew', element: <CrewPage /> },
+      { path: 'appointments-register', element: <MakeAppointmentPage /> },
+      { path: 'visits', element: <Visits />, children:[
+        { path: ':id', element: <DetailVisit />}
+      ] },
+      { path: 'recommendations', element: <RecommendationsPage />, children:[
+        { path: ':id', element: <RecommendationDetail />}
+      ] },
+      { path: 'auth', element: <LoginPage /> },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
