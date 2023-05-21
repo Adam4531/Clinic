22c9a8d4439614b70f297a0d93081ad59c942b4c
Norbi <norbert.nosek@student.uwm.edu.pl>
2023-05-21 16:40:25 +0200

    repair serializers

--- backend/backend/patients/serializers.py
+++ backend/backend/patients/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from datetime import datetime
+from datetime import date
 
 from ..patients.models import Patient, Allergy, Time_of_activity
 
@@ -28,7 +28,7 @@ class PatientSerializer(serializers.HyperlinkedModelSerializer):
             raise serializers.ValidationError("Field 'password' cannot have more than 255 characters!")
         if len(value['pesel']) != 11:
             raise serializers.ValidationError("Field 'pesel' has to be exactly 11 characters long!")
-        if len(value['phone_number']) != 11:
+        if len(value['phone_number']) != 9:
             raise serializers.ValidationError("Field 'phone_number' has to be exactly 11 characters long!")
         if (value['age']) <= 0:
             raise serializers.ValidationError("Field 'age' cannot be empty or negative number!")
@@ -45,7 +45,7 @@ class AllergySerializer(serializers.HyperlinkedModelSerializer):
     def validate(self, value):
         if value['name'] == 0:
             raise serializers.ValidationError("Field 'name' cannot be empty!")
-        if len(value['name'] > 45):
+        if len(value['name']) > 45:
             raise serializers.ValidationError("Field 'name' cannot have more than 11 characters!")
         return value
 
@@ -56,10 +56,10 @@ class TimeOfActivitySerializer(serializers.HyperlinkedModelSerializer):
         fields = "__all__"
 
     def validate(self, value):  # TODO Do we need additional validation here?
-        if value['date_start'] < datetime.now():  # TODO check if it's correct in logic way
+        if value['date_start'] < date.today():  # TODO check if it's correct in logic way
             raise serializers.ValidationError("Field 'date_start' cannot be placed in the future!")
         if value['date_start'] > value['date_end']:
             raise serializers.ValidationError("Field 'date_start' cannot be placed after date_end!")
-        if value['date_end'] < datetime.now():  # TODO check if it's correct in logic way
+        if value['date_end'] < date.today():  # TODO check if it's correct in logic way
             raise serializers.ValidationError("Field 'data_end' cannot be placed in the past!")
         return value
