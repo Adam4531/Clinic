87f8f19729842081590d2e07116d8c630b5230f6
Adrian <zirexxx2015@gmail.com>
2023-06-03 23:14:59 +0200

    Show profile data from API. Update not working: bad request

--- frontend/frontend/src/patient/EditProfile.js
+++ frontend/frontend/src/patient/EditProfile.js
@@ -1,55 +1,142 @@
-import { useState } from "react";
+import { useState, useTransition, useEffect } from "react";
 import SuccessEdit from "./SuccessEdit";
 import styles from "./EditProfile.module.css";
 
 function EditProfilePage(props) {  
   const [succesIsShown, setSuccesIsShown]=useState(false);
 
-  const showSuccesHandler = () =>{
-    setSuccesIsShown(true);
-  }
+  // Dane do pobrania:
+  const [email, setEmail] = useState('');
+  const [age, setAge] = useState('');
+  const [passwd, setPasswd] = useState('');
+  const [phone, setPhone] = useState('');
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
+  const [allergies, setAllergies] = useState([]);
+  const [drugs, setDrugs] = useState('');
+
+  // Pobieranie danych:
+  const handleEmailChange = (event) =>{
+    setEmail(event.target.value);
+  };
+  const handleAgeChange = (event) =>{
+    setAge(event.target.value);
+  };
+  const handlePasswdChange = (event) =>{
+    setPasswd(event.target.value);
+  };
+  const handlePhoneChange = (event) =>{
+    setPhone(event.target.value);
+  };
+  const handleFnameChange = (event) =>{
+    setFname(event.target.value);
+  };
+  const handleLnameChange = (event) =>{
+    setLname(event.target.value);
+  };
+  const handleAllergiesChange = (event) =>{
+    setAllergies(event.target.value);
+  };
+  const handleDrugsChange = (event) =>{
+    setDrugs(event.target.value);
+  };
+
   const hideSuccesHandler = () =>{
     setSuccesIsShown(false);
     console.log('dziala')
   }
+
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/auth/user/',
+        {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+            SameSite: "none",
+          },
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        console.log(data);
+      });
+  }, []);
+
+  //BAD REQUEST(400)
+  const showSuccesHandler = (event) => {
+    event.preventDefault();
+    const data = {
+      email: email,
+      age: age,
+      first_name: fname,
+      last_name: lname,
+      phone_number: phone,
+      allergies: allergies
+    };
+    fetch(`http://127.0.0.1:8000/auth/users/${localStorage.getItem("owner")}`,
+    {
+      method: "PATCH",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        SameSite: "none",
+      },
+      body: JSON.stringify(data)
+    })
+    .then((response) => {
+      if (response.status === 422 || response.status === 401) {
+        return response;
+      } else {
+        setSuccesIsShown(true);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  } 
+
     return (
         <div className={styles.container}>
           <div className={styles.edit_profile_window}>
             <form>
               <h3 className={styles.h3}>Profil</h3>
               <table className={styles.table}>
+                <tbody>
                 <tr>
                   <td>
-                    <input type="text" id="email-input" name="email" placeholder="Email"></input>
+                    <input defaultValue={user.email} type="text" id="email-input" name="email" onChange={handleEmailChange} placeholder="Email"></input>
                   </td>
                   <td>
-                    <input type="text" id="age-input" name="age" placeholder="Wiek"></input>
+                    <input defaultValue={user.age} type="text" id="age-input" name="age" onChange={handleAgeChange} placeholder="Wiek"></input>
                   </td>
                 </tr>
                 <tr>
                   <td>
-                    <input type="password" id="passwd-input" name="passwd" placeholder="Hasło"></input>
+                    <input type="password" id="passwd-input" name="passwd" onChange={handlePasswdChange} placeholder="Hasło"></input>
                   </td>
                   <td>
-                    <input type="text" id="phone-input" name="phone" placeholder="Numer telefonu"></input>
+                    <input defaultValue={user.phone_number} type="text" id="phone-input" name="phone" onChange={handlePhoneChange} placeholder="Numer telefonu"></input>
                   </td>
                 </tr>
                 <tr>
                   <td>
-                    <input type="text" id="name-input" name="name" placeholder="Imię"></input>
+                    <input defaultValue={user.first_name} type="text" id="name-input" name="name" onChange={handleFnameChange} placeholder="Imię"></input>
                   </td>
                   <td>
-                    <input type="text" id="surname-input" name="surname" placeholder="Nazwisko"></input>
+                    <input defaultValue={user.last_name} type="text" id="surname-input" name="surname" onChange={handleLnameChange} placeholder="Nazwisko"></input>
                   </td>
                 </tr>
                 <tr>
                   <td>
-                    <input type="text" id="allergies-input" name="allergies" placeholder="Alergie"></input>
+                    <input defaultValue={user.allergies} type="text" id="allergies-input" name="allergies" onChange={handleAllergiesChange} placeholder="Alergie"></input>
                   </td>
                   <td>
-                    <input type="text" id="drugs-input" name="drugs" placeholder="Stosowane leki"></input>
+                    <input type="text" id="drugs-input" name="drugs" onChange={handleDrugsChange} placeholder="Stosowane leki"></input>
                   </td>
                 </tr>
+                </tbody>
               </table>
               {succesIsShown && <SuccessEdit onHideCart={hideSuccesHandler}/>}
               <button className={styles.btn_save_changes} onClick={showSuccesHandler}>Zapisz</button>
