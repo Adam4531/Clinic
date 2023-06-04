6b7c7a452a269e11264354bd9ffe205380f47ce3
Adam4531 <70113220+Adam4531@users.noreply.github.com>
2023-06-04 10:08:12 +0200

    Commented Patient and Employee model deleted

--- backend/backend/employees/models.py
+++ backend/backend/employees/models.py
@@ -13,29 +13,3 @@ class Degree(models.Model):
 
     def __str__(self):
         return f'{self.name}'
-
-
-# class Employee(models.Model):
-#
-#     class DoctorSpecializationEnum(models.TextChoices):
-#         Family_doctor = 'Lekarz rodzinny'
-#         Pediatrician = 'Pediatra'
-#         Dermatologist = 'Dermatolog'
-#         Surgeon = 'Chirurg'
-#         Dentist = 'Stomatolog'
-#         Orthopaedist = 'Ortopeda'
-#         Internist = 'Internista'
-#
-#     employee_image = models.ImageField(upload_to='employees/')
-#     employed_at = models.DateField()
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-#     specialization = models.CharField(max_length=15, choices=DoctorSpecializationEnum.choices, blank=True, default="")
-#     title_of_degree = models.ForeignKey(Degree, on_delete=models.DO_NOTHING, blank=True, null=True)
-#     role = models.ForeignKey(Role, on_delete=models.DO_NOTHING, blank=True, null=True)
-#     # title_of_degree = models.CharField(max_length=100, blank=True, null=True)
-#     # role = models.CharField(max_length=13, choices=Role.choices, default=Role.Doctor, )
-#
-#     def __str__(self):
-#         return f'{self.title_of_degree} {self.role} {self.user}'
-
-
--- backend/backend/patients/models.py
+++ backend/backend/patients/models.py
@@ -15,15 +15,3 @@ class Allergy(models.Model):
 
     def __str__(self):
         return f'{self.name}, {self.time_of_activity}'
-
-
-# class Patient(models.Model):
-#     pesel = models.CharField(max_length=11, unique=True)
-#     phone_number = models.CharField(max_length=11, unique=True)
-#     age = models.IntegerField()
-#     allergies = models.ManyToManyField(Allergy, blank=True, null=True)
-#     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
-#
-#     def __str__(self):
-#         return f'{self.user}, {self.phone_number}, {self.age}'
-
