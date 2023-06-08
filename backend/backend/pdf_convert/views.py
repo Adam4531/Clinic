a1a6a566971691dc134bb59ae0a914756eda66c1
Adam4531 <70113220+Adam4531@users.noreply.github.com>
2023-06-08 18:52:49 +0200

    Showvisits by all/id, and createpdf by all/id views are working, minor issue with some special polish characters printed as black squares

--- backend/backend/pdf_convert/views.py
+++ backend/backend/pdf_convert/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from ..visits.models import Visit
 from ..employees.models import Role,Degree
 from ..authorization.models import User
@@ -5,35 +7,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa
+from datetime import date
 
 
 def show_visits(request):
-    visits = Visit.objects.all() #doctor=request.user
-    print(visits)
-    # user = User.objects.filter()
-    # data filter
+    today = date.today()
+    visits = Visit.objects.filter(date__year=today.year, date__month=today.month, date__day=today.day) #doctor=request.user
+    # print(visits)
 
 
     context = {
-        'visit': visits,
-        # 'user': user,
+        'visits': visits,
     }
 
     return render(request, 'main/showInfo.html', context)
 
 
 def pdf_report_create(request):
-    user = User.objects.filter()
-    visit = Visit.objects.filter()
-    #data filter
+    today = date.today()
+    visits = Visit.objects.filter(date__year=today.year, date__month=today.month,
+                                  date__day=today.day)  # doctor=request.user
+    # print(visits)
+
 
 
 
     template_path = 'main/pdfReport.html'
 
     context = {
-        'user': user,
-        'visit': visit
+        'visits': visits,
     }
 
     response = HttpResponse(content_type='application/pdf')
@@ -51,4 +53,44 @@ def pdf_report_create(request):
     if pisa_status.err:
        return HttpResponse('We had some errors <pre>' + html + '</pre>')
     return response
+
+
+def show_visits_by_id(request, id):
+    today = date.today()
+    visits = Visit.objects.filter(date__year=today.year, date__month=today.month, date__day=today.day, doctor__id=id) #doctor=request.user
+
+    context = {
+        'visits': visits,
+    }
+
+    return render(request, 'main/showInfo.html', context)
+
+
+def pdf_report_create_by_id(request, id):
+    today = date.today()
+    visits = Visit.objects.filter(date__year=today.year, date__month=today.month,
+                                  date__day=today.day, doctor__id=id)
+
+    template_path = 'main/pdfReport.html'
+
+    context = {
+        'visits': visits,
+    }
+
+    response = HttpResponse(content_type='application/pdf')
+
+    response['Content-Disposition'] = 'filename="products_report.pdf"'
+
+    template = get_template(template_path)
+
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
+
 
